c06eb711d962e8e0ba776aa10f5e6eef7a501749
Alexander Ekdahl <alexander@ekdahl.io>
2019-03-16 20:29:49 -0400

    fmt + terraform enterprise

--- main.tf
+++ main.tf
@@ -1,60 +1,58 @@
-variable "subdomain" {}
-variable "shared_secret" {}
-variable "username" {}
-variable "password" {}
+terraform {
+  backend "remote" {
+    organization = "ekdahl-io"
+
+    workspaces {
+      name = "main"
+    }
+  }
+}
 
 provider "aws" {
   region = "eu-central-1"
 }
 
 resource "aws_route53_zone" "primary" {
-   name = "ekdahl.io"
+  name = "ekdahl.io"
 }
 
 module "www" {
-  source = "./www"
+  source  = "./www"
   zone_id = "${aws_route53_zone.primary.zone_id}"
 }
-
-// module "vpn" {
-//   source = "./vpn"
-//   zone_id = "${aws_route53_zone.primary.zone_id}"
-//   subdomain = "${var.subdomain}"
-//   shared_secret = "${var.shared_secret}"
-//   username = "${var.username}"
-//   password = "${var.password}"
-// }
 
 // Zoho Email
 resource "aws_route53_record" "email" {
   zone_id = "${aws_route53_zone.primary.zone_id}"
-  name = "ekdahl.io"
-  ttl = 300
-  type = "MX"
+  name    = "ekdahl.io"
+  ttl     = 300
+  type    = "MX"
+
   records = [
     "10 mx.zoho.com.",
-    "20 mx2.zoho.com."
+    "20 mx2.zoho.com.",
   ]
 }
 
 // Zoho SPF and Keybase site verification
 resource "aws_route53_record" "txt" {
-   zone_id = "${aws_route53_zone.primary.zone_id}"
-   name = "ekdahl.io"
-   type = "TXT"
-   ttl = "300"
-   records = [
-    "v=spf1 mx include:zoho.com ~all", // Zoho
-    "keybase-site-verification=FTXUbkJFBxaXJlvrCSrVemhq1ls7gdvo0kG9CazTrc0" // Keybase
-  ]
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  name    = "ekdahl.io"
+  type    = "TXT"
+  ttl     = "300"
+
+  records = [
+    "v=spf1 mx include:zoho.com ~all",                                       // Zoho
+    "keybase-site-verification=FTXUbkJFBxaXJlvrCSrVemhq1ls7gdvo0kG9CazTrc0",
+  ] // Keybase
 }
 
 // DKIM
 resource "aws_route53_record" "dkim" {
   zone_id = "${aws_route53_zone.primary.zone_id}"
-  name = "ekdahl._domainkey.ekdahl.io"
-  type = "TXT"
-  ttl = "300"
+  name    = "ekdahl._domainkey.ekdahl.io"
+  type    = "TXT"
+  ttl     = "300"
   records = ["v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCALmYxFPwybwcJwiInzlzfE4c8JF3xk8KIprE0emXebqJEsmdfZ4ExEoDRmEryntzFHG9tASnKcQzPqeLadih3OD4F6DPhl3/A4tfifi8HGg7cIL7dqj3ixYqOxecehzKzq2ojvfL4zPfZdcmQx0hEY1OeNFWqp3rDMnVs1B93uwIDAQAB"]
 }
 
--- www/www.tf
+++ www/www.tf
@@ -10,7 +10,7 @@ data "template_file" "policy" {
 
 resource "aws_s3_bucket" "primary" {
   bucket = "ekdahl.io"
-  acl = "public-read"
+  acl    = "public-read"
   policy = "${data.template_file.policy.rendered}"
 
   website {
@@ -20,7 +20,7 @@ resource "aws_s3_bucket" "primary" {
 
 resource "aws_s3_bucket" "secondary" {
   bucket = "www.ekdahl.io"
-  acl = "public-read"
+  acl    = "public-read"
 
   website {
     redirect_all_requests_to = "ekdahl.io"
@@ -28,34 +28,36 @@ resource "aws_s3_bucket" "secondary" {
 }
 
 resource "aws_s3_bucket_object" "index" {
-  bucket = "${aws_s3_bucket.primary.id}"
-  key = "index.html"
-  content = "${file("${path.module}/index.html")}"
+  bucket       = "${aws_s3_bucket.primary.id}"
+  key          = "index.html"
+  content      = "${file("${path.module}/index.html")}"
   content_type = "text/html"
 }
 
 resource "aws_route53_record" "primary" {
   zone_id = "${var.zone_id}"
-  name = "ekdahl.io"
-  type = "A"
+  name    = "ekdahl.io"
+  type    = "A"
 
   alias {
     name = "${aws_s3_bucket.primary.website_domain}"
+
     //zone_id = "${aws_s3_bucket.primary.hosted_zone_id}"
-    zone_id = "Z21DNDUVLTQW6Q"
+    zone_id                = "Z21DNDUVLTQW6Q"
     evaluate_target_health = false
   }
 }
 
 resource "aws_route53_record" "secondary" {
   zone_id = "${var.zone_id}"
-  name = "www.ekdahl.io"
-  type = "A"
+  name    = "www.ekdahl.io"
+  type    = "A"
 
   alias {
     name = "${aws_s3_bucket.secondary.website_domain}"
+
     //zone_id = "${aws_s3_bucket.secondary.hosted_zone_id}"
-    zone_id = "Z21DNDUVLTQW6Q"
+    zone_id                = "Z21DNDUVLTQW6Q"
     evaluate_target_health = false
   }
 }
